f93ef9a8d31f2471995e71faee8ca2b77471c89a
hdgbdn <hdgbdn92@gmail.com>
2021-08-13 12:45:03 +0800

    now show the sky

---
+++ RayTracingInOneWeekend.cpp
@@ -0,0 +1,180 @@
+﻿#include <iostream>
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+#include "glm/glm.hpp"
+#include "ray.h"
+
+using namespace std;
+
+// configs
+const int window_width = 1024;
+const int window_height = 1024;
+
+const float aspect_ratio = static_cast<float>(window_width) / window_height;
+
+const char* vertexShaderSource =
+"#version 330 core\n"
+"layout(location = 0) in vec3 aPos;\n"
+"layout(location = 1) in vec2 aTexCoord;\n"
+
+"out vec2 TexCoord;\n"
+"void main()\n"
+"{\n"
+"    gl_Position = vec4(aPos, 1.0);\n"
+"    TexCoord = vec2(aTexCoord.x, aTexCoord.y);\n"
+"}\n";
+
+const char* fragmentShaderSource =
+"#version 330 core\n"
+"out vec4 FragColor;\n"
+
+"in vec2 TexCoord;\n"
+
+"uniform sampler2D texture1;\n"
+
+"void main()\n"
+"{\n"
+"    FragColor = texture(texture1, TexCoord);\n"
+"}\n";
+
+
+int main() {
+
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+    GLFWwindow* window = glfwCreateWindow(window_width, window_height, "Ray Tracing In OneWeekend", nullptr, nullptr);
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress)))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return 1;
+    }
+
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    // check for shader compile errors
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    // fragment shader
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    // check for shader compile errors
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    // link shaders
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    // check for linking errors
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    float quadVertices[] = {
+     -1.0f,  1.0f,  0.0f, 1.0f,
+     -1.0f, -1.0f,  0.0f, 0.0f,
+      1.0f, -1.0f,  1.0f, 0.0f,
+
+     -1.0f,  1.0f,  0.0f, 1.0f,
+      1.0f, -1.0f,  1.0f, 0.0f,
+      1.0f,  1.0f,  1.0f, 1.0f
+    };
+
+    unsigned int quadVAO, quadVBO;
+    glGenVertexArrays(1, &quadVAO);
+    glGenBuffers(1, &quadVBO);
+    glBindVertexArray(quadVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), static_cast<void*>(0));
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
+
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    auto* data = new unsigned char[window_height * window_width * 3];
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, window_width, window_height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+
+    glDisable(GL_DEPTH_TEST);
+
+    // rendering
+    auto ray_color = [](const ray& r)->glm::vec3
+    {
+        float product = glm::dot(glm::normalize(r.direction()), glm::vec3(0, 0, -1));
+        return glm::vec3(1.0, 1.0, product);
+    };
+
+    auto setPixelColor = [](int h, int w, unsigned char* p, const glm::vec3& col)
+    {
+        int index = 3 * (h * window_width + w);
+        unsigned char r = col.r * 255;
+        unsigned char g = col.g * 255;
+        unsigned char b = col.b * 255;
+        p[index++] = r;
+        p[index++] = g;
+        p[index++] = b;
+    };
+
+	// camera
+    auto origin = glm::vec3(0.f);
+    float focal_length = 1.0;
+    float screenHeight = 2;
+    float screenWidth = screenHeight * aspect_ratio;
+    auto lower_left_corner =
+        origin - glm::vec3(screenWidth / 2, .0f, .0f) - glm::vec3(.0f, screenHeight / 2, .0f) - glm::vec3(.0f, .0f, focal_length);
+	
+    while (!glfwWindowShouldClose(window))
+    {
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+    	
+        for (int j = window_height - 1; j >= 0; --j) {
+            for (int i = 0; i < window_width; ++i) {
+                float u = static_cast<float>(j) / window_height;
+                float v = static_cast<float>(i) / window_width;
+                glm::vec3 pixelPos =
+                    lower_left_corner + glm::vec3(0.f, u * screenHeight, 0.f) + glm::vec3(v * screenWidth, 0.f, 0.f);
+                glm::vec3 color = ray_color(ray(origin, pixelPos - origin));
+            	setPixelColor(j, i, data, color);
+            }
+        }
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, window_width, window_height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glUseProgram(shaderProgram);
+        glBindTexture(GL_TEXTURE_2D, texture);
+    	glBindVertexArray(quadVAO);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+}
